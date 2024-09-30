a5eb3fafa1a2920c49fdfb751f96a7f0855a2249
Yamelnyk <yachkana@ukr.net>
2024-09-30 12:32:13 +0300

    router

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/students.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -26,32 +26,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not faund',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contact with id {contactId}!',
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
