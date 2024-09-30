4dae5bf0ea7d8c359ec44a0657a285288bbdb100
Yamelnyk <yachkana@ukr.net>
2024-09-30 09:48:22 +0300

    fix status

--- src/server.js
+++ src/server.js
@@ -29,6 +29,7 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -46,6 +47,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
